2da2b348ed222d7318d1bcdde1f0bc0859ba83a9
Hairul Lana <hairullana99@gmail.com>
2021-02-11 00:45:18 +0800

    limit & money checking

---
+++ plugins/limit.js
@@ -0,0 +1,24 @@
+// let PhoneNumber = require('awesome-phonenumber')
+let handler = async (m, { conn, text }) => {
+	
+  // format mata uang
+  const format = num => {
+    const n = String(num),
+          p = n.indexOf('.')
+    return n.replace(
+        /\d(?=(?:\d{3})+(?:\.|$))/g,
+        (m, i) => p < 0 || i < p ? `${m},` : m
+    )
+  }
+
+	let money = format(global.DATABASE.data.users[m.sender].exp)
+  let limit = global.DATABASE.data.users[m.sender].limit
+	
+	conn.reply(m.chat, `*Ini Hartamu Sat !*\n\nUang : Rp. ${money}\nLimit : ${limit}`, m)
+}
+handler.help = ['limit','money']
+handler.tags = ['xp']
+handler.command = /^(limit|money)$/i
+handler.exp = 0
+handler.limit = false
+module.exports = handler
