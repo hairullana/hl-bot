c3fb7b9f9b0c75d3f9d40a9fd6763afd7c915114
Hairul Lana <hairullana99@gmail.com>
2021-07-01 21:06:06 +0800

    change selfmode

--- plugins/owner-selfmode.js
+++ plugins/owner-selfmode.js
@@ -3,10 +3,10 @@ let handler  = async (m, { conn, args }) => {
 		await conn.updatePresence(m.chat, Presence.composing) 
 		conn.reply(m.chat, `*Berikan keterangan on atau off*`, m)
 	} else if(args[0] == 'on') {
-    global.DATABASE.data.selfMode = true
+    global.DATABASE.data.maintenance = true
 		conn.reply(m.chat, `*❏ MAINTENANCE ON*`, m)
 	} else if(args[0] == 'off') {
-    global.DATABASE.data.selfMode = false
+    global.DATABASE.data.maintenance = false
 		conn.reply(m.chat, `*❏ MAINTENANCE OFF*`, m)
 	} else {
 		await conn.updatePresence(m.chat, Presence.composing) 
