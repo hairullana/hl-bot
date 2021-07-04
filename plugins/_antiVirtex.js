feef1b49be2dafda2b605933a03cf40376da7a8d
Hairul Lana <hairullana99@gmail.com>
2021-07-04 21:07:58 +0800

    fix antivirtex

--- plugins/_antiVirtex.js
+++ plugins/_antiVirtex.js
@@ -6,18 +6,20 @@ handler.before = async (m, {
   isAdmin
 }) => {
   if (m.isGroup && !isAdmin && antiVirtex && isBotAdmin && (m.text.match(/(৭৭৭৭৭৭৭৭|๒๒๒๒๒๒๒๒|๑๑๑๑๑๑๑๑|ดุท้่เึางืผิดุท้่เึางื|𐎑⃢𝘼𝙩𝙩𝙖𝙘𝙠|۩꦳|ผิดุท้เึางื)/gi) || m.text.length >= 1000)) {
-    conn.groupRemove(m.chat, [m.sender], m).then(() => {
-      conn.blockUser(m.sender, "add").then(() => {
-        conn.modifyChat(m.chat, 'delete').catch(console.log).then(() => {
-          conn.reply(m.chat, `\n`.repeat(100)).then(() => {
-            conn.reply(m.chat, `*Ada virtex, tanda telah dibaca dulu.*\n*Jangan lupa bersihkan chat.*`)
-          })
-        })
+    conn.groupRemove(m.chat, [m.sender], m)
+    conn.blockUser(m.sender, "add")
+    conn.modifyChat(m.chat, 'delete')
+    conn.reply(m.chat, `\n`.repeat(100)).then(() => {
+      conn.reply(m.chat, `*Ada virtex, tanda telah dibaca dulu.*\n*Jangan lupa bersihkan chat.*`)
+      conn.reply(owner[0] + "@s.whatsapp.net", `*❏ Virtex Detected from @${m.sender.split('@')[0]} on group ${conn.getGroup(m.chat)}*`, null, {
+        contextInfo: {
+          mentionedJid: [m.sender]
+        }
       })
     })
   } else if (!m.isGroup && (m.text.match(/(৭৭৭৭৭৭৭৭|๒๒๒๒๒๒๒๒|๑๑๑๑๑๑๑๑|ดุท้่เึางืผิดุท้่เึางื|𐎑⃢𝘼𝙩𝙩𝙖𝙘𝙠|۩꦳|ผิดุท้เึางื)/gi) || m.text.length >= 1000)) {
     conn.blockUser(m.sender, "add").then(() => {
-      conn.modifyChat(m.chat, 'delete').catch(console.log).then(() => {
+      conn.modifyChat(m.chat, 'delete').then(() => {
         conn.reply(owner[0] + "@s.whatsapp.net", `*❏ Virtex Detected From @${m.sender.split('@')[0]}*`, null, {
           contextInfo: {
             mentionedJid: [m.sender]
