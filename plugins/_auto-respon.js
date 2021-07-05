cf0afd582fbfa7da1c4045b680e18a938fa7d741
Hairul Lana <hairullana99@gmail.com>
2021-07-05 09:07:11 +0800

    fix autorespon

--- plugins/_auto-respon.js
+++ plugins/_auto-respon.js
@@ -2,13 +2,21 @@ let fs = require('fs')
 let handler = m => m
 handler.before = async (m, { conn, isMods }) => {
   if (m.text.toLowerCase() == "y" && isMods) {
-    let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
     ran = "./media/desah-bangsat.mp3"
     buffer = fs.readFileSync(ran)
-    const option = {
-      mimetype: 'audio/mp4',
-      ptt: true,
-      contextInfo: { mentionedJid: users }
+    let option
+    if (m.isGroup){
+      let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+      option = {
+        mimetype: 'audio/mp4',
+        ptt: true,
+        contextInfo: { mentionedJid: users }
+      }
+    }else {
+      option = {
+        mimetype: 'audio/mp4',
+        ptt: true
+      }
     }
     conn.voice(m.chat, buffer, option)
   }
