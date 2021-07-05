b0332cfd2104f451834f067b924beba277773f2a
Hairul Lana <hairullana99@gmail.com>
2021-07-05 09:11:26 +0800

    fix premium-check

--- plugins/_premium-check.js
+++ plugins/_premium-check.js
@@ -1,13 +1,12 @@
 let handler = m => m
 handler.before = async (m, { conn, isPrems }) => {
-  let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
 	if (isPrems) {
     if (new Date() * 1 >= global.DATABASE.data.users[m.sender].premiumDate) {
-      conn.reply(m.chat, "*Maaf waktu untuk status premium anda telah berakhir :(*\n*Chat owner untuk upgrade premium lagi*", m, { contextInfo: { mentionedJid: users } }).then(() => {
+      conn.reply(m.chat, "*Maaf waktu untuk status premium anda telah berakhir :(*\n*Chat owner untuk upgrade premium lagi*", m).then(() => {
         global.DATABASE.data.users[m.sender].premium = false
         let name = 'Hairul Lana'
         let number = global.owner[1]
-        conn.sendVcard(m.chat, name, number, null, { contextInfo: { mentionedJid: users } })
+        conn.sendVcard(m.chat, name, number)
       })
     }
   }
